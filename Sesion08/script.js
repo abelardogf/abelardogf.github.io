1d9e48d56dd7ce5cbd1e61cbbe969e887bf47e31
abelardogf <croopier@gmail.com>
2024-10-25 15:42:25 +0200

    clase

--- Sesion08/script.js
+++ Sesion08/script.js
@@ -1,6 +1,6 @@
 $(window).scroll(function(){
     //Si el elemento es visible
-    if ($(".cambio-background").visible(true)) {
+    if ($(".cambio-background ").visible(true)) {
         //Añade la clase “visible”
         $(".cambio-background").addClass("visible");
         } else {
